93997384187bb2f7374ff67ff9d038ba4182ef89
TIBkey <81963789+baokeyit1177key@users.noreply.github.com>
2024-06-20 17:45:10 +0700

    fix TypeOfMetalRepository

--- src/main/java/online/gemfpt/BE/Repository/TypeOfMetalRepository.java
+++ src/main/java/online/gemfpt/BE/Repository/TypeOfMetalRepository.java
@@ -2,13 +2,16 @@ package online.gemfpt.BE.Repository;
 
 import online.gemfpt.BE.entity.TypeOfMetal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface TypeOfMetalRepository extends JpaRepository<TypeOfMetal, Long> {
-    Optional<TypeOfMetal> findByMetalType(String metalType);
-    Optional<TypeOfMetal> findByMetalTypeAndStatus(String metalType, boolean status);
+   // Optional<TypeOfMetal> findByMetalType(String metalType);
+     @Query("SELECT t FROM TypeOfMetal t WHERE t.metalType = :metalType AND t.status = true")
+    Optional<TypeOfMetal> findByMetalType(@Param("metalType") String metalType);
 
 }
